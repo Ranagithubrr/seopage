1e33e21a3d1f99af254bf2e574940fe3eabb5eec
ranagithubrr <ranarr.dev@gmail.com>
2024-11-07 14:00:15 +0600

    Fix: box structure fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ import Card from "./components/card/Card";
 function App() {
   return (
     <>
-      <div className="grid grid-cols-4 gap-2 p-4">
-        <Card />   
+      <div className="flex overflow-x-scroll box-border">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </>
   );
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,7 +2,7 @@ import CardItem from "../items/CardItem";
 
 const Card = () => {
   return (
-    <div className="bg-gray-100 box-border px-2">
+    <div className="bg-gray-100 box-border px-2 min-w-[350px]">
       <div className="flex items-center justify-between px-4 py-3">
         <div className="flex items-center justify-center">
           <div className="bg-red-500 w-5 h-5 rounded-tl-full rounded-bl-full mr-2"></div>
@@ -12,7 +12,18 @@ const Card = () => {
           <span className="bg-gray-200 px-2 py-1 rounded">0</span>
         </div>
       </div>
+      <div className="overflow-y-scroll" style={{ maxHeight: `calc(100vh - 100px)` }}>
       <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      </div>
     </div>
   );
 };
