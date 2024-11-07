65628c9cee643cd8ae51158703294485ea0a6dff
ranagithubrr <ranarr.dev@gmail.com>
2024-11-07 14:28:46 +0600

    Feat: data fetched from backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import axios from "axios";
 import "./App.css";
 import Card from "./components/card/Card";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const FetchData = async () => {
+    const response = await axios.get("http://localhost:5000/todos");
+    const resdata = await response.data;
+    if (resdata) {
+      setData(resdata);
+    }
+  };
+  useEffect(() => {
+    FetchData();
+  }, []);
+
   return (
     <>
       <div className="flex overflow-x-scroll box-border">
-        <Card />
-        <Card />
+        <Card title="Incomplete" color="bg-red-500" />
+        <Card title="To Do" color="bg-blue-500" />
+        <Card title="Doing" color="bg-yellow-500" />
         <Card />
         <Card />
         <Card />
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,28 +1,35 @@
 import CardItem from "../items/CardItem";
 
-const Card = () => {
+const Card = ({ title, color }) => {
   return (
     <div className="bg-gray-100 box-border px-2 min-w-[350px]">
       <div className="flex items-center justify-between px-4 py-3">
         <div className="flex items-center justify-center">
-          <div className="bg-red-500 w-5 h-5 rounded-tl-full rounded-bl-full mr-2"></div>
-          <span>Incomplete</span>
+          <div
+            className={`${
+              color || "bg-red-500"
+            } w-5 h-5 rounded-tl-full rounded-bl-full mr-2`}
+          ></div>
+          <span>{title || "title"}</span>
         </div>
         <div>
           <span className="bg-gray-200 px-2 py-1 rounded">0</span>
         </div>
       </div>
-      <div className="overflow-y-scroll" style={{ maxHeight: `calc(100vh - 100px)` }}>
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
+      <div
+        className="overflow-y-scroll"
+        style={{ maxHeight: `calc(100vh - 100px)` }}
+      >
+        <CardItem />
+        <CardItem />
+        <CardItem />
+        <CardItem />
+        <CardItem />
+        <CardItem />
+        <CardItem />
+        <CardItem />
+        <CardItem />
+        <CardItem />
       </div>
     </div>
   );
